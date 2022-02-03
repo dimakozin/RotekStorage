bc8c7aaba1b6fd09abb970296786e22baaed1dfc
Dmitry <dimakozin@gmail.com>
2022-02-03 17:00:04 +0300

    Данные с сервера

--- src/App.vue
+++ src/App.vue
@@ -14,18 +14,37 @@ import LeftMenu from '@/components/LeftMenu.vue'
 import FindPanel from '@/components/FindPanel.vue';
 import Boxes from '@/components/Boxes.vue';
 import {mapActions} from 'vuex'
+import axios from 'axios'
 
 @Options({
   components: {
     LeftMenu, FindPanel, Boxes
   },
   methods: {
-    ...mapActions(['dropActiveElement']),
+    ...mapActions(['dropActiveElement', 'setStorage']),
   },
   mounted () {
     window.addEventListener('keydown', (ev) => {
       this.dropActiveElement()
     })
+
+    const GRAPHQLServerURL = 'http://localhost:3000/graphql'
+    axios.post(GRAPHQLServerURL, {
+      query: `query {
+        storage{
+          id
+          title
+          section
+          amount
+          boxId
+        }}`}).then( response => {
+          const storage = response.data.data.storage
+          this.setStorage(storage)
+
+    }).catch (error => {
+      console.error(error)
+    })
+
   }
 })
 
